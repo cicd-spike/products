1898d8d4cf38caccc32bacbbf4c69494853bab9d
Wes Anderson <wesley.herb.anderson@gmail.com>
2022-10-23 16:02:54 -0700

    feature: starter code

---
+++ server.js
@@ -0,0 +1,28 @@
+let express = require('express');
+let app = express();
+
+// testing again again
+
+app.get('/', function (req, res) {
+  res.json({
+    products: [
+      {
+        name: 'laptop',
+        price: 500,
+      },
+      {
+        name: 'monitor',
+        price: 100,
+      },
+      {
+        name: 'keyboard',
+        price: 30,
+      },
+    ],
+  });
+  res.end();
+});
+
+app.listen(3001, function () {
+  console.log('app listening on port 3001!');
+});
